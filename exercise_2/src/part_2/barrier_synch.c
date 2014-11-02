9e29922703ccac14a2171618027a38ef7f01d17c
introhpc03 <introhpc03@creek01.ziti.uni-heidelberg.de>
2014-11-02 16:33:20 +0100

    All-in-One

--- exercise_2/src/part_2/barrier_synch.c
+++ exercise_2/src/part_2/barrier_synch.c
@@ -1,14 +1,51 @@
+/*********************************************************************************
+ * FILENAME         barrier_synch.c
+ * 
+ * DESCRIPTION      These functions are part of the submission to exercises of 
+ *                  the "Introduction to High Percformance Computing" (Intro HPC) 
+ *                  lecture of the University of Heidelberg.
+ * 
+ *                  Exercise 2 - Implementation of centralized barrier in MPI.
+ * 
+ * AUTHORS          Klaus Naumann
+ *                  Christoph Klein
+ *                  Günther Schindler
+ *
+ * LAST CHANGE      02. NOV 2014
+ * 
+ ********************************************************************************/
 #include <stdio.h>
-#include <stdlib.h>
 #include "mpi.h"
-#include "cBarrier.h"
+#include <stdlib.h>
+
+void centralized_barrier( int *rank, int *size, int *counter ) {
+	int signal = 1, i;
+	
+	if( *rank != *(size)-1 ){
+		MPI_Send( &signal, 1, MPI_INT, *(size)-1, 1 , MPI_COMM_WORLD );
+	}
+
+	if( *rank == *(size)-1 ){
+		for( i=0 ; i<*(size)-1; i++ ) {
+			MPI_Recv( &signal, 1 ,MPI_INT, i, 1, MPI_COMM_WORLD, NULL );
+		}
+	}
+
+	if( *rank == *(size)-1 ){
+		*counter = !*counter;
+	}
+
+	MPI_Bcast(counter, 1, MPI_INT, *(size)-1, MPI_COMM_WORLD);
+
+	while(!*counter);
+	*counter = !*counter;
+}
 
 int main(int argc, char **argv) {
-
-	int i, rank, size;
+	int rank, size, i;
+	double t, t1, t2;
 	int iterations = atoi(argv[1]);
-	double t1, t2, t;
-	int do_ = 0;
+	int counter = 0;
 	
 	MPI_Init(&argc, &argv);
 
@@ -17,14 +54,15 @@ int main(int argc, char **argv) {
 
 	for( i=0; i<iterations; i++ ){
 		t1 = MPI_Wtime();
-		printf("do_ : %d\n", do_);
-		cBarrier(&rank, &size, &do_);
-		printf("do_ : %d\n", do_);
+		//MPI_Barrier(MPI_COMM_WORLD);
+		centralized_barrier(&rank, &size, &counter);
 		t2 = MPI_Wtime();
 		t += (t2 - t1);
-		printf("Process No. %d reached barrier. Time elapsed: %f\n", rank, t/iterations);
+		printf("Proc %d reached barrier %d\n", rank, i);
 	}
-	printf("Average barrier latency of process %d :  %f\n", rank, t/iterations);
+
+	printf("Time spent in barrier by process %d is %f\n", rank, t/iterations);
+
 	MPI_Finalize();
 	return 0;
 }
