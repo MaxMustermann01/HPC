13b0a7b51376dceae525f742757586022bbeea55
MaxMustermann01 <G.Schindler@stud.uni-heidelberg.de>
2016-01-19 22:47:05 +0100

    ...

--- halo_old/test/test_buffer_3D.cpp
+++ halo_old/test/test_buffer_3D.cpp
@@ -146,107 +146,106 @@ int main(int argc, char** argv) {
 
   /* Initializing receive buffers */
     /*Direct neighbours */
-  tb.init_recv_buffer(&kplus_r[0], MPI_INT, ( t+i+j+k ), 0,0,1);
-  tb.init_recv_buffer(&kminus_r[0], MPI_INT, ( t+i+j+k ), 0,0,-1);
-  tb.init_recv_buffer(&iplus_r[0], MPI_INT, ( t+i+j+k ), 1,0,0);
-  tb.init_recv_buffer(&iminus_r[0], MPI_INT, ( t+i+j+k ), -1,0,0);
-  tb.init_recv_buffer(&jplus_r[0], MPI_INT, ( t+i+j+k ), 0,1,0);
-  tb.init_recv_buffer(&jminus_r[0], MPI_INT, ( t+i+j+k ), 0,-1,0);
+  tb.init_recv_buffer(&kplus_r[0], MPI_INT, ( t+i+j+k+1 ), 0,0,1);
+  tb.init_recv_buffer(&kminus_r[0], MPI_INT, ( t+i+j+k-1 ), 0,0,-1);
+  tb.init_recv_buffer(&iplus_r[0], MPI_INT, ( t+i+j+k+1 ), 1,0,0);
+  tb.init_recv_buffer(&iminus_r[0], MPI_INT, ( t+i+j+k-1 ), -1,0,0);
+  tb.init_recv_buffer(&jplus_r[0], MPI_INT, ( t+i+j+k+1 ), 0,1,0);
+  tb.init_recv_buffer(&jminus_r[0], MPI_INT, ( t+i+j+k-1 ), 0,-1,0);
   
   /*Rest of the k=-1 plane */
-  tb.init_recv_buffer(&iplusjpluskminus_r[0], MPI_INT, ( t+i+j+k ), 1,1,-1);
+  tb.init_recv_buffer(&iplusjpluskminus_r[0], MPI_INT, ( t+i+j+k+1 ), 1,1,-1);
   tb.init_recv_buffer(&jpluskminus_r[0], MPI_INT, ( t+i+j+k ), 0,1,-1);
-  tb.init_recv_buffer(&iminusjpluskminus_r[0], MPI_INT, ( t+i+j+k ), -1,1,-1);
+  tb.init_recv_buffer(&iminusjpluskminus_r[0], MPI_INT, ( t+i+j+k-1 ), -1,1,-1);
   tb.init_recv_buffer(&ipluskminus_r[0], MPI_INT, ( t+i+j+k ), 1,0,-1);
-  tb.init_recv_buffer(&iminuskminus_r[0], MPI_INT, ( t+i+j+k ), -1,0,-1);
-  tb.init_recv_buffer(&iplusjminuskminus_r[0], MPI_INT, ( t+i+j+k ), 1,-1,-1);
-  tb.init_recv_buffer(&jminuskminus_r[0], MPI_INT, ( t+i+j+k ), 0,-1,-1);
-  tb.init_recv_buffer(&iminusjminuskminus_r[0], MPI_INT, ( t+i+j+k ), -1,-1,-1);
+  tb.init_recv_buffer(&iminuskminus_r[0], MPI_INT, ( t+i+j+k-2 ), -1,0,-1);
+  tb.init_recv_buffer(&iplusjminuskminus_r[0], MPI_INT, ( t+i+j+k-1 ), 1,-1,-1);
+  tb.init_recv_buffer(&jminuskminus_r[0], MPI_INT, ( t+i+j+k-2 ), 0,-1,-1);
+  tb.init_recv_buffer(&iminusjminuskminus_r[0], MPI_INT, ( t+i+j+k-3 ), -1,-1,-1);
   
   /*Rest of the k=0 plane */
-  tb.init_recv_buffer(&iplusjplus_r[0], MPI_INT, ( t+i+j+k ), 1,1,0);
+  tb.init_recv_buffer(&iplusjplus_r[0], MPI_INT, ( t+i+j+k+2 ), 1,1,0);
   tb.init_recv_buffer(&iminusjplus_r[0], MPI_INT, ( t+i+j+k ), -1,1,0);
   tb.init_recv_buffer(&iplusjminus_r[0], MPI_INT, ( t+i+j+k ), 1,-1,0);
-  tb.init_recv_buffer( &iminusjminus_r[0], MPI_INT, ( t+i+j+k ), -1,-1,0);
+  tb.init_recv_buffer( &iminusjminus_r[0], MPI_INT, ( t+i+j+k-2 ), -1,-1,0);
   
   /*Rest of the k=1 plane */
-  tb.init_recv_buffer(&iplusjpluskplus_r[0], MPI_INT, ( t+i+j+k ), 1,1,1);
-  tb.init_recv_buffer(&jpluskplus_r[0], MPI_INT, ( t+i+j+k ), 0,1,1);
-  tb.init_recv_buffer(&iminusjpluskplus_r[0], MPI_INT, ( t+i+j+k ), -1,1,1);
-  tb.init_recv_buffer(&ipluskplus_r[0], MPI_INT, ( t+i+j+k ), 1,0,1);
+  tb.init_recv_buffer(&iplusjpluskplus_r[0], MPI_INT, ( t+i+j+k+3 ), 1,1,1);
+  tb.init_recv_buffer(&jpluskplus_r[0], MPI_INT, ( t+i+j+k+2 ), 0,1,1);
+  tb.init_recv_buffer(&iminusjpluskplus_r[0], MPI_INT, ( t+i+j+k+1 ), -1,1,1);
+  tb.init_recv_buffer(&ipluskplus_r[0], MPI_INT, ( t+i+j+k+2 ), 1,0,1);
   tb.init_recv_buffer(&iminuskplus_r[0], MPI_INT, ( t+i+j+k ), -1,0,1);
-  tb.init_recv_buffer(&iplusjminuskplus_r[0], MPI_INT, ( t+i+j+k ), 1,-1,1);
+  tb.init_recv_buffer(&iplusjminuskplus_r[0], MPI_INT, ( t+i+j+k+1 ), 1,-1,1);
   tb.init_recv_buffer(&jminuskplus_r[0], MPI_INT, ( t+i+j+k ), 0,-1,1);
-  tb.init_recv_buffer(&iminusjminuskplus_r[0], MPI_INT, ( t+i+j+k ), -1,-1,1);
+  tb.init_recv_buffer(&iminusjminuskplus_r[0], MPI_INT, ( t+i+j+k-1 ), -1,-1,1);
   
   /* Fill the send buffers with the unique process id */
     /* Direct neighbours */
-std::fill(&kplus[0], &kplus[ t+i+j+k ], pid );
-std::fill(&kminus[0], &kminus[t+i+j+k], pid );
-std::fill(&iplus[0], &iplus[t+i+j+k], pid );
-std::fill(&iminus[0], &iminus[t+i+j+k], pid );
-std::fill(&jplus[0], &jplus[t+i+j+k], pid );
-std::fill(&jminus[0], &jminus[t+i+j+k], pid );
+  std::fill(&kplus[0], &kplus[ t+i+j+k ], pid );
+  std::fill(&kminus[0], &kminus[t+i+j+k], pid );
+  std::fill(&iplus[0], &iplus[t+i+j+k], pid );
+  std::fill(&iminus[0], &iminus[t+i+j+k], pid );
+  std::fill(&jplus[0], &jplus[t+i+j+k], pid );
+  std::fill(&jminus[0], &jminus[t+i+j+k], pid );
   /* Rest of the k=-1 plane */
-std::fill(&iplusjpluskminus[0], &iplusjpluskminus[t+i+j+k], pid);
-std::fill(&jpluskminus[0], &jpluskminus[t+i+j+k], pid);
-std::fill(&iminusjpluskminus[0], &iminusjpluskminus[t+i+j+k], pid);
-std::fill(&ipluskminus[0], &ipluskminus[t+i+j+k], pid);
-std::fill(&iminuskminus[0], &iminuskminus[t+i+j+k], pid);
-std::fill(&iplusjminuskminus[0], &iplusjminuskminus[t+i+j+k], pid);
-std::fill(&jminuskminus[0], &jminuskminus[t+i+j+k], pid);
-std::fill(&iminusjminuskminus[0], &iminusjminuskminus[t+i+j+k], pid);
+  std::fill(&iplusjpluskminus[0], &iplusjpluskminus[t+i+j+k], pid);
+  std::fill(&jpluskminus[0], &jpluskminus[t+i+j+k], pid);
+  std::fill(&iminusjpluskminus[0], &iminusjpluskminus[t+i+j+k], pid);
+  std::fill(&ipluskminus[0], &ipluskminus[t+i+j+k], pid);
+  std::fill(&iminuskminus[0], &iminuskminus[t+i+j+k], pid);
+  std::fill(&iplusjminuskminus[0], &iplusjminuskminus[t+i+j+k], pid);
+  std::fill(&jminuskminus[0], &jminuskminus[t+i+j+k], pid);
+  std::fill(&iminusjminuskminus[0], &iminusjminuskminus[t+i+j+k], pid);
   /* Rest of the k=0 plane */
-std::fill(&iplusjplus[0], &iplusjplus[t+i+j+k], pid);
-std::fill(&iminusjplus[0], &iminusjplus[t+i+j+k], pid);
-std::fill(&iplusjminus[0], &iplusjminus[t+i+j+k], pid);
-std::fill(&iminusjminus[0], &iminusjminus[t+i+j+k], pid);
+  std::fill(&iplusjplus[0], &iplusjplus[t+i+j+k], pid);
+  std::fill(&iminusjplus[0], &iminusjplus[t+i+j+k], pid);
+  std::fill(&iplusjminus[0], &iplusjminus[t+i+j+k], pid);
+  std::fill(&iminusjminus[0], &iminusjminus[t+i+j+k], pid);
   /* Rest of the k=1 plane */
-std::fill(&iplusjpluskplus[0], &iplusjpluskplus[t+i+j+k], pid);
-std::fill(&jpluskplus[0], &jpluskplus[t+i+j+k], pid);
-std::fill(&iminusjpluskplus[0], &iminusjpluskplus[t+i+j+k], pid);
-std::fill(&ipluskplus[0], &ipluskplus[t+i+j+k], pid);
-std::fill(&iminuskplus[0], &iminuskplus[t+i+j+k], pid);
-std::fill(&iplusjminuskplus[0], &iplusjminuskplus[t+i+j+k], pid);
-std::fill(&jminuskplus[0], &jminuskplus[t+i+j+k], pid);
-std::fill(&iminusjminuskplus[0], &iminusjminuskplus[t+i+j+k], pid);
+  std::fill(&iplusjpluskplus[0], &iplusjpluskplus[t+i+j+k], pid);
+  std::fill(&jpluskplus[0], &jpluskplus[t+i+j+k], pid);
+  std::fill(&iminusjpluskplus[0], &iminusjpluskplus[t+i+j+k], pid);
+  std::fill(&ipluskplus[0], &ipluskplus[t+i+j+k], pid);
+  std::fill(&iminuskplus[0], &iminuskplus[t+i+j+k], pid);
+  std::fill(&iplusjminuskplus[0], &iplusjminuskplus[t+i+j+k], pid);
+  std::fill(&jminuskplus[0], &jminuskplus[t+i+j+k], pid);
+  std::fill(&iminusjminuskplus[0], &iminusjminuskplus[t+i+j+k], pid);
   
   tb.do_receives();
   tb.do_sends();
   tb.do_waits();
   
-  
   /* Data to reference the received data */
   /* Direct neighbours */
-  std::vector<int> res_kplus_r( t+i+j+k );
-  std::vector<int> res_kminus_r ( t+i+j+k );
-  std::vector<int> res_iplus_r( t+i+j+k );
-  std::vector<int> res_iminus_r( t+i+j+k );
-  std::vector<int> res_jplus_r( t+i+j+k );
-  std::vector<int> res_jminus_r( t+i+j+k );
+  std::vector<int> res_kplus_r( t+i+j+k+1 );
+  std::vector<int> res_kminus_r ( t+i+j+k-1 );
+  std::vector<int> res_iplus_r( t+i+j+k+1 );
+  std::vector<int> res_iminus_r( t+i+j+k-1 );
+  std::vector<int> res_jplus_r( t+i+j+k+1 );
+  std::vector<int> res_jminus_r( t+i+j+k-1 );
   /* Rest of the k=-1 plane */
-  std::vector<int> res_iplusjpluskminus_r( t+i+j+k );
+  std::vector<int> res_iplusjpluskminus_r( t+i+j+k+1 );
   std::vector<int> res_jpluskminus_r( t+i+j+k );
-  std::vector<int> res_iminusjpluskminus_r( t+i+j+k );
+  std::vector<int> res_iminusjpluskminus_r( t+i+j+k-1 );
   std::vector<int> res_ipluskminus_r( t+i+j+k );
-  std::vector<int> res_iminuskminus_r( t+i+j+k );
-  std::vector<int> res_iplusjminuskminus_r( t+i+j+k );
-  std::vector<int> res_jminuskminus_r( t+i+j+k );
-  std::vector<int> res_iminusjminuskminus_r( t+i+j+k );
+  std::vector<int> res_iminuskminus_r( t+i+j+k-2 );
+  std::vector<int> res_iplusjminuskminus_r( t+i+j+k-1 );
+  std::vector<int> res_jminuskminus_r( t+i+j+k-2 );
+  std::vector<int> res_iminusjminuskminus_r( t+i+j+k-3 );
   /* Rest of the k=0 plane */
-  std::vector<int> res_iplusjplus_r( t+i+j+k );
+  std::vector<int> res_iplusjplus_r( t+i+j+k+2 );
   std::vector<int> res_iminusjplus_r( t+i+j+k );
   std::vector<int> res_iplusjminus_r( t+i+j+k );
-  std::vector<int> res_iminusjminus_r( t+i+j+k );
+  std::vector<int> res_iminusjminus_r( t+i+j+k-2 );
   /* Rest of the k=1 plane */
-  std::vector<int> res_iplusjpluskplus_r( t+i+j+k );
-  std::vector<int> res_jpluskplus_r( t+i+j+k );
-  std::vector<int> res_iminusjpluskplus_r( t+i+j+k );
-  std::vector<int> res_ipluskplus_r( t+i+j+k );
+  std::vector<int> res_iplusjpluskplus_r( t+i+j+k+3 );
+  std::vector<int> res_jpluskplus_r( t+i+j+k+2 );
+  std::vector<int> res_iminusjpluskplus_r( t+i+j+k+1 );
+  std::vector<int> res_ipluskplus_r( t+i+j+k+2 );
   std::vector<int> res_iminuskplus_r( t+i+j+k );
-  std::vector<int> res_iplusjminuskplus_r( t+i+j+k );
+  std::vector<int> res_iplusjminuskplus_r( t+i+j+k+1 );
   std::vector<int> res_jminuskplus_r( t+i+j+k );
-  std::vector<int> res_iminusjminuskplus_r( t+i+j+k );
+  std::vector<int> res_iminusjminuskplus_r( t+i+j+k-1 );
   
 //   std::vector<int> res_iminus_r( t+i+j-1 );
 //   std::vector<int> res_iplus_r( t+i+j+1 );
@@ -257,37 +256,37 @@ std::fill(&iminusjminuskplus[0], &iminusjminuskplus[t+i+j+k], pid);
 //   std::vector<int> res_iminusjplus_r( t+i+j );
 //   std::vector<int> res_iplusjplus_r( t+i+j+2 );
   
-    /* Fill the reference receive buffers with the expected process id */
-    /* Direct neighbours */
-std::fill(&res_kplus_r[0], &res_kplus_r[ t+i+j+k ], tb.getprocid(0,0,1));
-std::fill(&res_kminus_r[0], &res_kminus_r[t+i+j+k], tb.getprocid(0,0,-1));
-std::fill(&res_iplus_r[0], &res_iplus_r[t+i+j+k], tb.getprocid(1,0,0));
-std::fill(&res_iminus_r[0], &res_iminus_r[t+i+j+k], tb.getprocid(-1,0,0));
-std::fill(&res_jplus_r[0], &res_jplus_r[t+i+j+k], tb.getprocid(0,1,0));
-std::fill(&res_jminus_r[0], &res_jminus_r[t+i+j+k], tb.getprocid(0,-1,0));
+  /* Fill the reference receive buffers with the expected process id */
+  /* Direct neighbours */
+  std::fill(&res_kplus_r[0], &res_kplus_r[ t+i+j+k ], tb.getprocid(0,0,1));
+  std::fill(&res_kminus_r[0], &res_kminus_r[t+i+j+k], tb.getprocid(0,0,-1));
+  std::fill(&res_iplus_r[0], &res_iplus_r[t+i+j+k], tb.getprocid(1,0,0));
+  std::fill(&res_iminus_r[0], &res_iminus_r[t+i+j+k], tb.getprocid(-1,0,0));
+  std::fill(&res_jplus_r[0], &res_jplus_r[t+i+j+k], tb.getprocid(0,1,0));
+  std::fill(&res_jminus_r[0], &res_jminus_r[t+i+j+k], tb.getprocid(0,-1,0));
   /* Rest of the k=-1 plane */
-std::fill(&res_iplusjpluskminus_r[0], &res_iplusjpluskminus_r[t+i+j+k], tb.getprocid(1,1,-1));
-std::fill(&res_jpluskminus_r[0], &res_jpluskminus_r[t+i+j+k], tb.getprocid(0,1,-1));
-std::fill(&res_iminusjpluskminus_r[0], &res_iminusjpluskminus_r[t+i+j+k], tb.getprocid(-1,1,-1));
-std::fill(&res_ipluskminus_r[0], &res_ipluskminus_r[t+i+j+k], tb.getprocid(1,0,-1));
-std::fill(&res_iminuskminus_r[0], &res_iminuskminus_r[t+i+j+k], tb.getprocid(-1,0,-1));
-std::fill(&res_iplusjminuskminus_r[0], &res_iplusjminuskminus_r[t+i+j+k], tb.getprocid(1,-1,-1));
-std::fill(&res_jminuskminus_r[0], &res_jminuskminus_r[t+i+j+k], tb.getprocid(0,-1,-1));
-std::fill(&res_iminusjminuskminus_r[0], &res_iminusjminuskminus_r[t+i+j+k], tb.getprocid(-1,-1,-1));
+  std::fill(&res_iplusjpluskminus_r[0], &res_iplusjpluskminus_r[t+i+j+k], tb.getprocid(1,1,-1));
+  std::fill(&res_jpluskminus_r[0], &res_jpluskminus_r[t+i+j+k], tb.getprocid(0,1,-1));
+  std::fill(&res_iminusjpluskminus_r[0], &res_iminusjpluskminus_r[t+i+j+k], tb.getprocid(-1,1,-1));
+  std::fill(&res_ipluskminus_r[0], &res_ipluskminus_r[t+i+j+k], tb.getprocid(1,0,-1));
+  std::fill(&res_iminuskminus_r[0], &res_iminuskminus_r[t+i+j+k], tb.getprocid(-1,0,-1));
+  std::fill(&res_iplusjminuskminus_r[0], &res_iplusjminuskminus_r[t+i+j+k], tb.getprocid(1,-1,-1));
+  std::fill(&res_jminuskminus_r[0], &res_jminuskminus_r[t+i+j+k], tb.getprocid(0,-1,-1));
+  std::fill(&res_iminusjminuskminus_r[0], &res_iminusjminuskminus_r[t+i+j+k], tb.getprocid(-1,-1,-1));
   /* Rest of the k=0 plane */
-std::fill(&res_iplusjplus_r[0], &res_iplusjplus_r[t+i+j+k], tb.getprocid(1,1,0));
-std::fill(&res_iminusjplus_r[0], &res_iminusjplus_r[t+i+j+k], tb.getprocid(-1,1,0));
-std::fill(&res_iplusjminus_r[0], &res_iplusjminus_r[t+i+j+k], tb.getprocid(1,-1,0));
-std::fill(&res_iminusjminus_r[0], &res_iminusjminus_r[t+i+j+k], tb.getprocid(-1,-1,0));
+  std::fill(&res_iplusjplus_r[0], &res_iplusjplus_r[t+i+j+k], tb.getprocid(1,1,0));
+  std::fill(&res_iminusjplus_r[0], &res_iminusjplus_r[t+i+j+k], tb.getprocid(-1,1,0));
+  std::fill(&res_iplusjminus_r[0], &res_iplusjminus_r[t+i+j+k], tb.getprocid(1,-1,0));
+  std::fill(&res_iminusjminus_r[0], &res_iminusjminus_r[t+i+j+k], tb.getprocid(-1,-1,0));
   /* Rest of the k=1 plane */
-std::fill(&res_iplusjpluskplus_r[0], &res_iplusjpluskplus_r[t+i+j+k], tb.getprocid(1,1,1));
-std::fill(&res_jpluskplus_r[0], &res_jpluskplus_r[t+i+j+k], tb.getprocid(0,1,1));
-std::fill(&res_iminusjpluskplus_r[0], &res_iminusjpluskplus_r[t+i+j+k], tb.getprocid(-1,1,1));
-std::fill(&res_ipluskplus_r[0], &res_ipluskplus_r[t+i+j+k], tb.getprocid(1,0,1));
-std::fill(&res_iminuskplus_r[0], &res_iminuskplus_r[t+i+j+k], tb.getprocid(-1,0,1));
-std::fill(&res_iplusjminuskplus_r[0], &res_iplusjminuskplus_r[t+i+j+k], tb.getprocid(1,-1,1));
-std::fill(&res_jminuskplus_r[0], &res_jminuskplus_r[t+i+j+k], tb.getprocid(0,-1,1));
-std::fill(&res_iminusjminuskplus_r[0], &res_iminusjminuskplus_r[t+i+j+k], tb.getprocid(-1,-1,1));
+  std::fill(&res_iplusjpluskplus_r[0], &res_iplusjpluskplus_r[t+i+j+k], tb.getprocid(1,1,1));
+  std::fill(&res_jpluskplus_r[0], &res_jpluskplus_r[t+i+j+k], tb.getprocid(0,1,1));
+  std::fill(&res_iminusjpluskplus_r[0], &res_iminusjpluskplus_r[t+i+j+k], tb.getprocid(-1,1,1));
+  std::fill(&res_ipluskplus_r[0], &res_ipluskplus_r[t+i+j+k], tb.getprocid(1,0,1));
+  std::fill(&res_iminuskplus_r[0], &res_iminuskplus_r[t+i+j+k], tb.getprocid(-1,0,1));
+  std::fill(&res_iplusjminuskplus_r[0], &res_iplusjminuskplus_r[t+i+j+k], tb.getprocid(1,-1,1));
+  std::fill(&res_jminuskplus_r[0], &res_jminuskplus_r[t+i+j+k], tb.getprocid(0,-1,1));
+  std::fill(&res_iminusjminuskplus_r[0], &res_iminusjminuskplus_r[t+i+j+k], tb.getprocid(-1,-1,1));
 
 //   std::fill(&res_iminus_r[0], &res_iminus_r[t+i+j-1], proc_grid.proc<-1,0>());
 //   std::fill(&res_iplus_r[0], &res_iplus_r[t+i+j+1], proc_grid.proc<1,0>());
