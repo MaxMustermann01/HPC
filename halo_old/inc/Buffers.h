d28fb371221fc4e22b9332142f8636108647f5b9
MaxMustermann01 <G.Schindler@stud.uni-heidelberg.de>
2016-01-29 18:16:23 +0100

    ...

--- halo_old/inc/Buffers.h
+++ halo_old/inc/Buffers.h
@@ -26,7 +26,35 @@ public:
     }
     
     /** 
-     * Return the associated buffer for given relative coordinates in three dimensions
+     * Returns the associated buffer for given relative coordinates in one dimensions
+     * 
+     * @param x relative x-coordinate of type integer
+     * @return reference of a pointer to a char (i.e. the specific buffer) 
+     */  
+    char*& buffer(int x) {
+      if (x < -1 || x > 1) {
+	std::cout << "Wrong index: must be -1 <= i <= 1!" << std::endl;
+      }
+      return m_buffers[ x+1 ];
+    }
+    
+    /** 
+     * Returns the associated buffer for given relative coordinates in two dimensions
+     * 
+     * @param x relative x-coordinate of type integer
+     * @param y relative y-coordinate of type integer
+     * @return reference of a pointer to a char (i.e. the specific buffer) 
+     */      
+    char*& buffer(int x, int y) {
+      if (x<-1 || y<-1|| x>1 || y>1) {
+	std::cout << "Wrong index: must be -1 <= i <= 1!" << std::endl;
+      }
+      return m_buffers[(x+1)*3 + y+1 ];
+      
+    }
+    
+    /** 
+     * Returns the associated buffer for given relative coordinates in three dimensions
      * 
      * @param x relative x-coordinate of type integer
      * @param y relative y-coordinate of type integer
@@ -41,30 +69,41 @@ public:
     }
     
     /** 
-     * Return the associated buffer for given relative coordinates in two dimensions
-     * 
-     * @param x relative x-coordinate of type integer
-     * @param y relative y-coordinate of type integer
-     * @return reference of a pointer to a char (i.e. the specific buffer) 
-     */      
-    char*& buffer(int x, int y) {
-      if (x<-1 || y<-1|| x>1 || y>1) {
-	std::cout << "Wrong index: must be -1 <= i <= 1!" << std::endl;
-      }
-      return m_buffers[(x+1)*3 + y+1 ]; 
-    }
-    
-    char*& buffer(int y) { 
-      return m_buffers[ y+1 ]; 
-    }
-    
-    /** 
      * Returns reference to the used MPI datatype 
      * 
      * @return m_dataype a reference to an MPI_Datatype
      */
     MPI_Datatype& datatype(void) { 
       return m_datatype; 
+    }
+    
+    /** 
+     * Returns a reference to the size of the associated buffer for given relative coordinates in one dimensions
+     * 
+     * @param x relative x-coordinate of type integer
+     * @return reference of an integer, the respective buffer size 
+     */
+    int& size(int x) { 
+      if (x < -1 || x > 1) {
+	std::cout << "Wrong index: must be -1 <= i <= 1!" << std::endl;
+      } 
+      return m_size[ x+1 ]; 
+      
+    }
+    
+    /** 
+     * Returns a reference to the size of the associated buffer for given relative coordinates in two dimensions
+     * 
+     * @param x relative x-coordinate of type integer
+     * @param y relative y-coordinate of type integer
+     * @return reference of an integer, the respective buffer size 
+     */
+    int& size(int x, int y) { 
+      if (x < -1 || y < -1 || x > 1 || y > 1) {
+	std::cout << "Wrong index: must be -1 <= i <= 1!" << std::endl;
+      } 
+      return m_size[ (x+1)*3 + y+1 ]; 
+      
     }
     
     /** 
@@ -80,23 +119,32 @@ public:
 	std::cout << "Wrong index: must be -1 <= i <= 1!" << std::endl;
       } 
       return m_size[ (x+1)*9 + (y+1)*3 + z+1 ];
+      
     }
     
-    /** 
-     * Returns a reference to the size of the associated buffer for given relative coordinates in two dimensions
-     * 
-     * @param x relative x-coordinate of type integer
-     * @param y relative y-coordinate of type integer
-     * @return reference of an integer, the respective buffer size 
-     */
-    int& size(int x, int y) { 
+    int getsize(int x) { 
+      if (x < -1 || x > 1) {
+	std::cout << "Wrong index: must be -1 <= i <= 1!" << std::endl;
+      } 
+      return m_size[ x+1 ]; 
+      
+    }
+    
+    int getsize(int x, int y) { 
       if (x < -1 || y < -1 || x > 1 || y > 1) {
 	std::cout << "Wrong index: must be -1 <= i <= 1!" << std::endl;
       } 
       return m_size[ (x+1)*3 + y+1 ]; 
+      
     }
     
-    int& size(int y) { return m_size[ y+1 ]; }
+    int getsize(int x, int y, int z) { 
+      if (x < -1 || y < -1 || z < -1 || x > 1 || y > 1 || z > 1) {
+	std::cout << "Wrong index: must be -1 <= i <= 1!" << std::endl;
+      } 
+      return m_size[ (x+1)*9 + (y+1)*3 + z+1 ];
+      
+    }
     
   };
 
